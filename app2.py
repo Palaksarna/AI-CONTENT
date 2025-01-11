57fa35d516e6d2ec168b4436fdfd675182c27760
Palak Sarna <palaksarna4@gmail.com>
2025-01-11 10:23:22 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import nltk
-nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('punkt_tab')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 import heapq
